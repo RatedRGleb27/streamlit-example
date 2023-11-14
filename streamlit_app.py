385711a63091fa5682f547fcc31660f664e49ffd
RatedRGleb27 <150341427+RatedRGleb27@users.noreply.github.com>
2023-11-14 14:18:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,25 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+# Import necessary libraries
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-"""
-# Welcome to Streamlit!
+# Set a title for your app
+st.title("My Streamlit App")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Add a text message
+st.write("Welcome to my Streamlit app!")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Add a slider widget
+age = st.slider("Select your age:", 0, 100, 25)
+st.write("Your selected age:", age)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# Create a simple plot based on the age
+x = np.arange(age)
+y = x * 2  # A simple linear relationship for demonstration
+plt.plot(x, y)
+plt.xlabel("Age")
+plt.ylabel("Double Age")
+plt.title("Age vs. Double Age Plot")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# Display the plot in Streamlit
+st.pyplot()
